b081366bed7ddabbfd4084b3d07ab45928d5abad
Celeste Peña <76794488+cmpena25@users.noreply.github.com>
2021-01-18 20:25:43 -0600

    created inline and external javascript html pages

---
+++ js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+alert("Message goes here.");
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log('The result' + confirmed);
+var userInput = prompt('Please type something')
+console.log('The use entered: ' +userInput)
