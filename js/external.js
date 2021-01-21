e6a23df24bf39a4d9ff3f3c363f26fafa9373ec3
Celeste Peña <76794488+cmpena25@users.noreply.github.com>
2021-01-21 16:41:25 -0600

    javascript exercise for functions

--- js/external.js
+++ js/external.js
@@ -2,4 +2,15 @@ console.log('hello from external');
 alert('Welcome to my website');
 var userAnswer = prompt('What is your favorite color?');
 alert(userAnswer);
+var userQuestion = prompt('What is your favorite number?');
+alert(userQuestion);
 
+var pricePerDay = 3;
+var userDaysRentedLM = prompt('How many days did you rent Little Mermaid?');
+console.log(userDaysRentedLM);
+var userDaysRentedBB = prompt('How many days did you rent Brother Bear?');
+console.log(userDaysRentedBB);
+var userDaysRentedH = prompt('How many days did you rent Hercules?');
+console.log(userDaysRentedH);
+var total = (userDaysRentedLM * pricePerDay) + (userDaysRentedBB * pricePerDay) + (userDaysRentedH * pricePerDay);
+alert(total);
